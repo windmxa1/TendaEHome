9a34db3a7c91ab612aab7fd5361da559c115d6e7
Marshall <windmxa1@qq.com>
2017-06-10 14:12:07 +0800

    添加ChangeTime工具类

--- src/org/test/Test04.java
+++ src/org/test/Test04.java
@@ -1,18 +1,23 @@
 package org.test;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.util.HibernateSessionFactory;
-import org.util.MD5;
-import org.view.VOrders;
-import org.view.VOrdersId;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Test04 {
 	public static void main(String[] args) {
-		System.out.println(MD5.string2MD5("123456"));
+		SimpleDateFormat spf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar c = Calendar.getInstance();
+		c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
+				c.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+		Date beginOfDate = c.getTime();
+		String begin = spf.format(beginOfDate);
+		System.out.println(begin);
+
+		c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
+				c.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+		Date endOfDate = c.getTime();
+		String end = spf.format(endOfDate);
+		System.out.println(end);
 	}
 }
